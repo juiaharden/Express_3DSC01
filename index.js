91cc74b60a005bfbb5ba0975351a47994f3f98a1
jujoca <130366314+juiaharden@users.noreply.github.com>
2024-10-04 14:59:09 +0000

    Criadas as rotas home e obre, iniciado o menu

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
 
-const PORT = 3301;
+const PORT = 3303;
 
 app.get("/", function (req, res) {
-  //res.sendFile()
-  res.send(__dirname);
+  res.sendFile(__dirname + "/src/views/index.html");
 });
+
+app.get("/sobre", function (req, res) {
+  res.sendFile(__dirname + "/src/views/about.html");
+});
+//ROTA UTILIZANO MIDDLEWRe (404)]
+app.use(function (req, res) {
+  res.status(404).sendFile(__dirname + "/src/views/404.html");
+});
+
 app.listen(PORT, () => {
   console.log("running...");
 });
